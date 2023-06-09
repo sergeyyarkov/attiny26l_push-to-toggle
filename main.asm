953b7886db1b5b49b0f860f74613bec57dd0658b
sergeyyarkov <serzh.yarkov@gmail.com>
2023-06-09 23:05:42 +0300

    three buttons

--- main.asm
+++ main.asm
@@ -1,14 +1,23 @@
 .INCLUDE "tn26def.inc"
 .LIST
 
-; **** LABLES ****************************************************
+; **** REGISTERS *************************************************
 
-.DEF  SW_FLAGS    = r20
 .DEF  PC_HISTORY  = r19
 
-.EQU  LED_PORT    = PA7
-.EQU  SW_PORT     = PA6
+; **** LABLES ****************************************************
 
+.EQU  LED_PORT    = PORTA
+.EQU  LED_PIN     = PINA
+.EQU  LED_0_PIN   = PA7
+.EQU  LED_1_PIN   = PA6
+.EQU  LED_2_PIN   = PA5
+
+.EQU  SW_PORT     = PORTA
+.EQU  SW_PIN      = PINA
+.EQU  SW_0_PIN    = PA4
+.EQU  SW_1_PIN    = PA3
+.EQU  SW_2_PIN    = PA2
 
 ; **** CODE SEGMENT **********************************************
 
@@ -24,45 +33,67 @@ rjmp  RESET_vect
 rjmp PIN_CHANGE_vect
 
 PIN_CHANGE_vect:
+  push  r16
   push  r17
   in    r17, SREG
 
-  in r18, PINA
-  eor r18, PC_HISTORY
-  in PC_HISTORY, PINA
+  in    r18, PINA
+  eor   r18, PC_HISTORY
+  in    PC_HISTORY, PINA
 
-  sbrs  r18, SW_PORT
-  rjmp  _sw_exit
-  rjmp  _sw_0_handler
+  _sw_0_check:
+    sbrs    r18, SW_0_PIN
+    rjmp    _sw_1_check
+    rjmp    _sw_0_handler
 
   _sw_0_handler:
-  sbis  PINA, SW_PORT
-  rcall TOGGLE_LED
-  ; then exit
+    rcall   DEBOUNCE_SW
+    ldi     r16, LED_0_PIN
+    sbis    SW_PIN, SW_0_PIN
+    rcall   TOGGLE_LED
+    rjmp    _sw_exit
+
+  _sw_1_check:
+    sbrs    r18, SW_1_PIN
+    rjmp    _sw_2_check
+    rjmp    _sw_1_handler
+
+  _sw_1_handler:
+    rcall   DEBOUNCE_SW
+    ldi     r16, LED_1_PIN
+    sbis    SW_PIN, SW_1_PIN
+    rcall   TOGGLE_LED
+    rjmp    _sw_exit
+
+  _sw_2_check:
+    sbrs    r18, SW_2_PIN
+    rjmp    _sw_exit
+
+  _sw_2_handler:
+    rcall   DEBOUNCE_SW
+    ldi     r16, LED_2_PIN
+    sbis    SW_PIN, SW_2_PIN
+    rcall   TOGGLE_LED
 
   _sw_exit:
-  out   SREG, r17
+    out   SREG, r17
+
   pop   r17
+  pop   r16
 reti
 
 RESET_vect:
   ldi   r16,    RAMEND
   out   SP,     r16
 
-
 MCU_INIT:
-  ldi   r16,    (1<<LED_PORT) | (0<<SW_PORT)
+  ldi   r16,    (1<<LED_0_PIN) | (1<<LED_1_PIN) | (1<<LED_2_PIN) | (0<<SW_0_PIN) | (0<<SW_1_PIN) | (0<<SW_2_PIN)
   out   DDRA,   r16
 
-  ldi   r16,    (1<<SW_PORT)
+  ldi   r16,    (1<<SW_0_PIN) | (1<<SW_1_PIN) | (1<<SW_2_PIN)
   out   PORTA,  r16
 
-  ; First, turn off comparator to enable pin change interrupt (only for tiny26l)
-  ldi   r16,    (1<<ACD)
-  out   ACSR,   r16
-
-  ; Enable pin change interrupt
-  ldi   r16,    (1<<PCIE1)
+  ldi   r16,    (1<<PCIE0)
   out   GIMSK,  r16
 
   ldi   PC_HISTORY, 0xff
@@ -71,21 +102,21 @@ MCU_INIT:
 
 LOOP:
   nop
-rjmp LOOP
+rjmp    LOOP
 
-delay:              ; FOR 1 MHz
-  push r16
-  push r17
+DEBOUNCE_SW:              ; FOR 1 MHz
+  push    r16
+  push    r17
 
-  ldi r17, 255
-    _loop_0:
-      ldi r16, 100
-      _dec_0:
-      dec r16
-      brne _dec_0
-    _loop_1:
-      dec r17
-      brne _loop_0
+  ldi     r17, 255
+  _loop_0:
+    ldi     r16, 150
+    _dec_0:
+    dec     r16
+    brne    _dec_0
+  _loop_1:
+    dec     r17
+    brne    _loop_0
 
   pop r17
   pop r16
@@ -94,13 +125,21 @@ ret
 TOGGLE_LED:
   push  r16
   push  r17
+  push  r18
 
   in    r17,    PINA
 
-  ldi   r16,    (1<<LED_PORT)
+  mov   r18,    r16
+  ldi   r16,    0x01
+  _toggle_led_shift:
+    lsl   r16
+    dec   r18
+    brne  _toggle_led_shift
+
   eor   r17,    r16
   out   PORTA,  r17
   
+  pop   r18
   pop   r17
   pop   r16
 ret
--- main.pro
+++ main.pro
@@ -1,8 +1,13 @@
 /avr_sim_project_file
 /home/sergeyyarkov/Documents/Projects/Firmwares/AVR/attiny26l_push-to-toggle/main.asm
 mainasm=main.asm
+file=main.lst,60,1,1
 file=main.asm,1,1,1
-clock=0, Div8
+clock=1000000, Div8
 device-type=ATtiny26
-windows=acepstwz
-alert1=0,0,-1,0,,0,0
+device-package=32-pin-MLF
+windows=acePstwz
+alert1=0,0,-1,-1,,0,0
+adcvref=5.0000
+adcvop=5.0000
+adcch=0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000,0.0000
